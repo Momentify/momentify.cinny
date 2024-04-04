dfeb66bfc1cb6f97d8b7a2d86737c49eae73e2f9
centino90 <anthonyjayansit2326282@gmail.com>
2024-04-05 00:35:35 +0800

    add back button

--- src/app/organisms/loading-room/LoadingRoom.jsx
+++ src/app/organisms/loading-room/LoadingRoom.jsx
@@ -2,6 +2,31 @@ import React from 'react';
 import './LoadingRoom.scss';
 
 import { LoadingScreen } from '../../templates/auth/Auth';
+import BackButton from '../../../momentify/BackButton';
+import theme from '../../../momentify/theme';
+
+const BackButtonContainer = {
+  "position": "absolute",
+  "top": "16px",
+  "left": "16px"
+};
+
+const Section = {
+  "background": theme.colors.backgroundOverlay,
+	"backgroundColor": "rgba(17, 17, 31, 1)",
+	"backgroundBlendMode": "multiply",
+	"color": theme.colors.softWhite,
+	"flex": 1,
+  "display": "flex",
+  "flexDdirection": "column",
+  "alignItems": "center",
+  "justifyContent": "center",
+  "textAlign": "center",
+  "padding": "1rem",
+  "gap": "20px",
+  "position": "relative",
+  "height": "100%"
+};
 
 function LoadingRoom() {
 
@@ -13,9 +38,12 @@ function LoadingRoom() {
   // , []);
 
   return (
-    <div className="app-welcome flex--center">
-      <LoadingScreen message='Please wait while we set up the chat room...' />
-    </div>
+      <section style={{...Section}}>    
+        <div style={{...BackButtonContainer}}>
+          <BackButton />
+        </div>
+        <LoadingScreen message='Please wait while we set up the chat room...' />
+      </section>
   );
 }
 
