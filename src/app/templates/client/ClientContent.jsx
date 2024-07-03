64858997d1ca45920d620e3353aad36749673c8c
JoseMagIII <josemaglaqueiii@gmail.com>
2024-07-03 19:13:16 +0800

    possible infinite loading fix

--- src/app/templates/client/ClientContent.jsx
+++ src/app/templates/client/ClientContent.jsx
@@ -25,51 +25,58 @@ export function ClientContent() {
   const mx = initMatrix.matrixClient;
 
   useEffect(() => {
-    const roomIDParam = extractRoomIDFromURL(window.location.href) ?? extractRoomIDFromURLWithoutParam(window.location.href) ?? '';
-    return () => {
-      if(!roomIDParam) {
-        setHasDbRecord(false)
-        return
-      }
+    const roomIDParam =
+      extractRoomIDFromURL(window.location.href) ??
+      extractRoomIDFromURLWithoutParam(window.location.href) ??
+      '';
 
-      if(!hasRoomInCache) {
-        return
-      }
-
-      getRoomByRoomAddress(getSecret(cons.secretKey.ACCESS_TOKEN), roomIDParam ?? null)
-        .then(dbRoom => {
-          if(!dbRoom?.room_address || !dbRoom?.room_name) {
-            setHasDbRecord(false)
-            return
-          }        
-          setJoinedRoom(dbRoom.room_address, localStorage.getItem(cons.secretKey.USER_ID)).then(res => {
-            console.log('setJoinedRoomSuccess', res)
-          }).catch(err => {
-            console.error('setJoinedRoomError', err)
-          })  
-          roomActions.join(dbRoom.room_address)
-            .then(res => {              
-              selectRoom(res);
-            })
-            .catch(err => {
-              setHasDbRecord(false)
-              console.error('roomActions.join.error', err)
-            })          
-        })
-        .catch(err => {
-          setHasDbRecord(false)
-          console.error('getRoomByRoomAddress.error', err)
-        })
+    if (!roomIDParam) {
+      setHasDbRecord(false);
+      return;
     }
-  }, [hasRoomInCache])
 
+    // if (!hasRoomInCache) {
+    //   return;
+    // }
+
+    getRoomByRoomAddress(getSecret(cons.secretKey.ACCESS_TOKEN), roomIDParam ?? null)
+      .then((dbRoom) => {
+        console.log(dbRoom);
+        if (!dbRoom?.room_address || !dbRoom?.room_name) {
+          setHasDbRecord(false);
+          return;
+        }
+
+        setJoinedRoom(dbRoom.room_address, getSecret(cons.secretKey.USER_ID))
+          .then((res) => {
+            console.log('setJoinedRoomSuccess', res);
+          })
+          .catch((err) => {
+            console.error('setJoinedRoomError', err);
+          });
+        roomActions
+          .join(dbRoom.room_address)
+          .then((res) => {
+            selectRoom(res);
+          })
+          .catch((err) => {
+            setHasDbRecord(false);
+            console.error('roomActions.join.error', err);
+          });
+      })
+      .catch((err) => {
+        setHasDbRecord(false);
+        console.error('getRoomByRoomAddress.error', err);
+      });
+  }, [hasRoomInCache]);
 
   useEffect(() => {
     const handleRoomSelected = (rId, pRoomId, eId) => {
       roomInfo.roomTimeline?.removeInternalListeners();
       const r = mx.getRoom(rId);
-      if(!r) {
-        setHasRoomInCache(false)
+
+      if (!r) {
+        setHasRoomInCache(false);
         setRoomInfo({
           room: null,
           eventId: null,
@@ -77,33 +84,34 @@ export function ClientContent() {
         return;
       }
       getRoomByRoomAddress(getSecret(cons.secretKey.ACCESS_TOKEN), r?.roomId ?? null)
-        .then(res => {
+        .then((res) => {
           // console.log('trigger', res) // Reminder: don't remove this console.log
           if (r && !!res?.room_name) {
-            r.room_avatar = res?.event_image ?? null
-            r.name = res?.room_event_name ?? res.room_name ?? r.name
+            r.room_avatar = res?.event_image ?? null;
+            r.name = res?.room_event_name ?? res.room_name ?? r.name;
             setRoomInfo({
               room: r,
               eventId: eId ?? null,
-            });                
+            });
           } else if (r && !res?.room_name) {
-            r.room_avatar = res?.event_image ?? null
+            r.room_avatar = res?.event_image ?? null;
             setRoomInfo({
               room: r,
               eventId: eId ?? null,
-            });          
+            });
           } else {
             setRoomInfo({
               room: null,
               eventId: null,
             });
-          }      
+          }
         })
-        .catch(err => {
-          setHasDbRecord(false)
-          console.error('getRoomByRoomAddress.error', err)
-        })
+        .catch((err) => {
+          setHasDbRecord(false);
+          console.error('getRoomByRoomAddress.error', err);
+        });
     };
+
     navigation.on(cons.events.navigation.ROOM_SELECTED, handleRoomSelected);
     return () => {
       navigation.removeListener(cons.events.navigation.ROOM_SELECTED, handleRoomSelected);
@@ -112,13 +120,13 @@ export function ClientContent() {
 
   const { room, eventId } = roomInfo;
 
-  if(!hasDbRecord) {
-    return <ErrorPage errorMessage={"Something went wrong. Please go back and try again."}/>;
+  if (!hasDbRecord) {
+    return <ErrorPage errorMessage={'Something went wrong. Please go back and try again.'} />;
   }
 
   if (!room) {
     setTimeout(() => openNavigation());
-    return <Welcome />;    
+    return <Welcome />;
   }
 
   return <RoomBaseView room={room} eventId={eventId} />;
